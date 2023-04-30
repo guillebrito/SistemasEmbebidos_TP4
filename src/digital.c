d1b1e15a2154c78621fcfaaa2c83123717f42a64
Guillermo Nicolas Brito <guillermonbrito@gmail.com>
2023-04-30 01:22:21 -0300

    Implementación de una única salida

--- src/digital.c
+++ src/digital.c
@@ -28,6 +28,7 @@ SPDX-License-Identifier: MIT
 /* === Headers files inclusions =============================================================== */
 
 #include <stdint.h>
+#include "chip.h"
 #include "digital.h"
 
 /* === Macros definitions ====================================================================== */
@@ -55,22 +56,27 @@ struct digital_output_s
 
 digital_output_t DigitalOutputCreate(uint8_t port, uint8_t pin)
 {
-    return 0;
+    static struct digital_output_s output;
+
+    output.port = port;
+    output.pin = pin;
+    Chip_GPIO_SetPinState(LPC_GPIO_PORT, output.port, output.pin, false);
+    Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, output.port, output.pin, true);
+
+    return &output;
 }
 
 void DigitalOutputActivate(digital_output_t output)
 {
-    return 0;
 }
 
 void DigitalOutputDeativate(digital_output_t output)
 {
-    return 0;
 }
 
 void DigitalOutputToggle(digital_output_t output)
 {
-    return 0;
+    Chip_GPIO_SetPinToggle(LPC_GPIO_PORT, output->port, output->pin);
 }
 
 /* === End of documentation ==================================================================== */
--- src/main.c
+++ src/main.c
@@ -40,70 +40,71 @@
 
 /* === Headers files inclusions =============================================================== */
 
+#include "digital.h"
 #include "chip.h"
 #include <stdbool.h>
 
 /* === Macros definitions ====================================================================== */
 
 #define LED_R_PORT 2
-#define LED_R_PIN 0
+#define LED_R_PIN  0
 #define LED_R_FUNC SCU_MODE_FUNC4
 #define LED_R_GPIO 5
-#define LED_R_BIT 0
+#define LED_R_BIT  0
 
 #define LED_G_PORT 2
-#define LED_G_PIN 1
+#define LED_G_PIN  1
 #define LED_G_FUNC SCU_MODE_FUNC4
 #define LED_G_GPIO 5
-#define LED_G_BIT 1
+#define LED_G_BIT  1
 
 #define LED_B_PORT 2
-#define LED_B_PIN 2
+#define LED_B_PIN  2
 #define LED_B_FUNC SCU_MODE_FUNC4
 #define LED_B_GPIO 5
-#define LED_B_BIT 2
+#define LED_B_BIT  2
 
 #define LED_1_PORT 2
-#define LED_1_PIN 10
+#define LED_1_PIN  10
 #define LED_1_FUNC SCU_MODE_FUNC0
 #define LED_1_GPIO 0
-#define LED_1_BIT 14
+#define LED_1_BIT  14
 
 #define LED_2_PORT 2
-#define LED_2_PIN 11
+#define LED_2_PIN  11
 #define LED_2_FUNC SCU_MODE_FUNC0
 #define LED_2_GPIO 1
-#define LED_2_BIT 11
+#define LED_2_BIT  11
 
 #define LED_3_PORT 2
-#define LED_3_PIN 12
+#define LED_3_PIN  12
 #define LED_3_FUNC SCU_MODE_FUNC0
 #define LED_3_GPIO 1
-#define LED_3_BIT 12
+#define LED_3_BIT  12
 
 #define TEC_1_PORT 1
-#define TEC_1_PIN 0
+#define TEC_1_PIN  0
 #define TEC_1_FUNC SCU_MODE_FUNC0
 #define TEC_1_GPIO 0
-#define TEC_1_BIT 4
+#define TEC_1_BIT  4
 
 #define TEC_2_PORT 1
-#define TEC_2_PIN 1
+#define TEC_2_PIN  1
 #define TEC_2_FUNC SCU_MODE_FUNC0
 #define TEC_2_GPIO 0
-#define TEC_2_BIT 8
+#define TEC_2_BIT  8
 
 #define TEC_3_PORT 1
-#define TEC_3_PIN 2
+#define TEC_3_PIN  2
 #define TEC_3_FUNC SCU_MODE_FUNC0
 #define TEC_3_GPIO 0
-#define TEC_3_BIT 9
+#define TEC_3_BIT  9
 
 #define TEC_4_PORT 1
-#define TEC_4_PIN 6
+#define TEC_4_PIN  6
 #define TEC_4_FUNC SCU_MODE_FUNC0
 #define TEC_4_GPIO 1
-#define TEC_4_BIT 9
+#define TEC_4_BIT  9
 
 /* === Private data type declarations ========================================================== */
 
@@ -119,7 +120,9 @@
 
 /* === Public function implementation ========================================================= */
 
-int main(void) {
+int main(void)
+{
+    digital_output_t led_verde;
 
     int divisor = 0;
     bool current_state, last_state = false;
@@ -146,8 +149,7 @@ int main(void) {
     Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, LED_2_GPIO, LED_2_BIT, true);
 
     Chip_SCU_PinMuxSet(LED_3_PORT, LED_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_3_FUNC);
-    Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_3_GPIO, LED_3_BIT, false);
-    Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, LED_3_GPIO, LED_3_BIT, true);
+    led_verde = DigitalOutputCreate(LED_3_GPIO, LED_3_PIN);
 
     /******************/
     Chip_SCU_PinMuxSet(TEC_1_PORT, TEC_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_1_FUNC);
@@ -162,34 +164,44 @@ int main(void) {
     Chip_SCU_PinMuxSet(TEC_4_PORT, TEC_4_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_4_FUNC);
     Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, TEC_4_GPIO, TEC_4_BIT, false);
 
-    while (true) {
-        if (Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, TEC_1_GPIO, TEC_1_BIT) == 0) {
+    while (true)
+    {
+        if (Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, TEC_1_GPIO, TEC_1_BIT) == 0)
+        {
             Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_B_GPIO, LED_B_BIT, true);
-        } else {
+        }
+        else
+        {
             Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_B_GPIO, LED_B_BIT, false);
         }
 
         current_state = (Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, TEC_2_GPIO, TEC_2_BIT) == 0);
-        if ((current_state) && (!last_state)) {
+        if ((current_state) && (!last_state))
+        {
             Chip_GPIO_SetPinToggle(LPC_GPIO_PORT, LED_1_GPIO, LED_1_BIT);
         }
         last_state = current_state;
 
-        if (Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, TEC_3_GPIO, TEC_3_BIT) == 0) {
+        if (Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, TEC_3_GPIO, TEC_3_BIT) == 0)
+        {
             Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_2_GPIO, LED_2_BIT, true);
         }
-        if (Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, TEC_4_GPIO, TEC_4_BIT) == 0) {
+        if (Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, TEC_4_GPIO, TEC_4_BIT) == 0)
+        {
             Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_2_GPIO, LED_2_BIT, false);
         }
 
         divisor++;
-        if (divisor == 1) {
+        if (divisor == 5)
+        {
             divisor = 0;
-            Chip_GPIO_SetPinToggle(LPC_GPIO_PORT, LED_3_GPIO, LED_3_BIT);
+            DigitalOutputToggle(led_verde);
         }
 
-        for (int index = 0; index < 100; index++) {
-            for (int delay = 0; delay < 25000; delay++) {
+        for (int index = 0; index < 100; index++)
+        {
+            for (int delay = 0; delay < 25000; delay++)
+            {
                 __asm("NOP");
             }
         }
