dd7d9cab84a988990fe24c869e89bfb7a5f0ea07
Guillermo Nicolas Brito <guillermonbrito@gmail.com>
2023-05-01 22:07:04 -0300

    Implementación completa de la bsp

--- src/bsp.c
+++ src/bsp.c
@@ -31,8 +31,69 @@ SPDX-License-Identifier: MIT
 /* === Headers files inclusions =============================================================== */
 
 #include "bsp.h"
+#include "chip.h"
 
 /* === Macros definitions ====================================================================== */
+
+#define LED_R_PORT 2
+#define LED_R_PIN  0
+#define LED_R_FUNC SCU_MODE_FUNC4
+#define LED_R_GPIO 5
+#define LED_R_BIT  0
+
+#define LED_G_PORT 2
+#define LED_G_PIN  1
+#define LED_G_FUNC SCU_MODE_FUNC4
+#define LED_G_GPIO 5
+#define LED_G_BIT  1
+
+#define LED_B_PORT 2
+#define LED_B_PIN  2
+#define LED_B_FUNC SCU_MODE_FUNC4
+#define LED_B_GPIO 5
+#define LED_B_BIT  2
+
+#define LED_1_PORT 2
+#define LED_1_PIN  10
+#define LED_1_FUNC SCU_MODE_FUNC0
+#define LED_1_GPIO 0
+#define LED_1_BIT  14
+
+#define LED_2_PORT 2
+#define LED_2_PIN  11
+#define LED_2_FUNC SCU_MODE_FUNC0
+#define LED_2_GPIO 1
+#define LED_2_BIT  11
+
+#define LED_3_PORT 2
+#define LED_3_PIN  12
+#define LED_3_FUNC SCU_MODE_FUNC0
+#define LED_3_GPIO 1
+#define LED_3_BIT  12
+
+#define TEC_1_PORT 1
+#define TEC_1_PIN  0
+#define TEC_1_FUNC SCU_MODE_FUNC0
+#define TEC_1_GPIO 0
+#define TEC_1_BIT  4
+
+#define TEC_2_PORT 1
+#define TEC_2_PIN  1
+#define TEC_2_FUNC SCU_MODE_FUNC0
+#define TEC_2_GPIO 0
+#define TEC_2_BIT  8
+
+#define TEC_3_PORT 1
+#define TEC_3_PIN  2
+#define TEC_3_FUNC SCU_MODE_FUNC0
+#define TEC_3_GPIO 0
+#define TEC_3_BIT  9
+
+#define TEC_4_PORT 1
+#define TEC_4_PIN  6
+#define TEC_4_FUNC SCU_MODE_FUNC0
+#define TEC_4_GPIO 1
+#define TEC_4_BIT  9
 
 /* === Private data type declarations ========================================================== */
 
@@ -52,6 +113,42 @@ static struct board_s board = {0};
 
 board_t BoardCreate(void)
 {
+    Chip_SCU_PinMuxSet(LED_R_PORT, LED_R_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_R_FUNC);
+    board.led_r = DigitalOutputCreate(LED_R_GPIO, LED_R_BIT, false);
+    // DigitalOutputDeactivate(board.led_r);
+
+    Chip_SCU_PinMuxSet(LED_G_PORT, LED_G_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_G_FUNC);
+    board.led_g = DigitalOutputCreate(LED_G_GPIO, LED_G_BIT, false);
+    // DigitalOutputDeactivate(board.led_g);
+
+    Chip_SCU_PinMuxSet(LED_B_PORT, LED_B_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_B_FUNC);
+    board.led_b = DigitalOutputCreate(LED_B_GPIO, LED_B_BIT, false);
+
+    /******************/
+
+    Chip_SCU_PinMuxSet(LED_1_PORT, LED_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_1_FUNC);
+    board.led_1 = DigitalOutputCreate(LED_1_GPIO, LED_1_BIT, false);
+
+    Chip_SCU_PinMuxSet(LED_2_PORT, LED_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_2_FUNC);
+    board.led_2 = DigitalOutputCreate(LED_2_GPIO, LED_2_BIT, false);
+
+    Chip_SCU_PinMuxSet(LED_3_PORT, LED_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_3_FUNC);
+    board.led_3 = DigitalOutputCreate(LED_3_GPIO, LED_3_BIT, false);
+
+    /******************/
+
+    Chip_SCU_PinMuxSet(TEC_1_PORT, TEC_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_1_FUNC);
+    board.tec_1 = DigitalInputCreate(TEC_1_GPIO, TEC_1_BIT, true);
+
+    Chip_SCU_PinMuxSet(TEC_2_PORT, TEC_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_2_FUNC);
+    board.tec_2 = DigitalInputCreate(TEC_2_GPIO, TEC_2_BIT, true);
+
+    Chip_SCU_PinMuxSet(TEC_3_PORT, TEC_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_3_FUNC);
+    board.tec_3 = DigitalInputCreate(TEC_3_GPIO, TEC_3_BIT, true);
+
+    Chip_SCU_PinMuxSet(TEC_4_PORT, TEC_4_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_4_FUNC);
+    board.tec_4 = DigitalInputCreate(TEC_4_GPIO, TEC_4_BIT, true);
+
     return &board;
 }
 
--- src/main.c
+++ src/main.c
@@ -42,69 +42,10 @@
 
 #include "digital.h"
 #include "chip.h"
+#include "bsp.h"
 #include <stdbool.h>
 
 /* === Macros definitions ====================================================================== */
-
-#define LED_R_PORT 2
-#define LED_R_PIN  0
-#define LED_R_FUNC SCU_MODE_FUNC4
-#define LED_R_GPIO 5
-#define LED_R_BIT  0
-
-#define LED_G_PORT 2
-#define LED_G_PIN  1
-#define LED_G_FUNC SCU_MODE_FUNC4
-#define LED_G_GPIO 5
-#define LED_G_BIT  1
-
-#define LED_B_PORT 2
-#define LED_B_PIN  2
-#define LED_B_FUNC SCU_MODE_FUNC4
-#define LED_B_GPIO 5
-#define LED_B_BIT  2
-
-#define LED_1_PORT 2
-#define LED_1_PIN  10
-#define LED_1_FUNC SCU_MODE_FUNC0
-#define LED_1_GPIO 0
-#define LED_1_BIT  14
-
-#define LED_2_PORT 2
-#define LED_2_PIN  11
-#define LED_2_FUNC SCU_MODE_FUNC0
-#define LED_2_GPIO 1
-#define LED_2_BIT  11
-
-#define LED_3_PORT 2
-#define LED_3_PIN  12
-#define LED_3_FUNC SCU_MODE_FUNC0
-#define LED_3_GPIO 1
-#define LED_3_BIT  12
-
-#define TEC_1_PORT 1
-#define TEC_1_PIN  0
-#define TEC_1_FUNC SCU_MODE_FUNC0
-#define TEC_1_GPIO 0
-#define TEC_1_BIT  4
-
-#define TEC_2_PORT 1
-#define TEC_2_PIN  1
-#define TEC_2_FUNC SCU_MODE_FUNC0
-#define TEC_2_GPIO 0
-#define TEC_2_BIT  8
-
-#define TEC_3_PORT 1
-#define TEC_3_PIN  2
-#define TEC_3_FUNC SCU_MODE_FUNC0
-#define TEC_3_GPIO 0
-#define TEC_3_BIT  9
-
-#define TEC_4_PORT 1
-#define TEC_4_PIN  6
-#define TEC_4_FUNC SCU_MODE_FUNC0
-#define TEC_4_GPIO 1
-#define TEC_4_BIT  9
 
 /* === Private data type declarations ========================================================== */
 
@@ -124,88 +65,40 @@ int main(void)
 {
     int divisor = 0;
 
-    digital_output_t led_r;
-    digital_output_t led_g;
-    digital_output_t led_b;
-    digital_output_t led_1;
-    digital_output_t led_2;
-    digital_output_t led_3;
-
-    digital_input_t tec_1;
-    digital_input_t tec_2;
-    digital_input_t tec_3;
-    digital_input_t tec_4;
-
-    /******************/
-
-    Chip_SCU_PinMuxSet(LED_R_PORT, LED_R_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_R_FUNC);
-    led_r = DigitalOutputCreate(LED_R_GPIO, LED_R_BIT, false);
-    DigitalOutputDeactivate(led_r);
-
-    Chip_SCU_PinMuxSet(LED_G_PORT, LED_G_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_G_FUNC);
-    led_g = DigitalOutputCreate(LED_G_GPIO, LED_G_BIT, false);
-    DigitalOutputDeactivate(led_g);
-
-    Chip_SCU_PinMuxSet(LED_B_PORT, LED_B_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_B_FUNC);
-    led_b = DigitalOutputCreate(LED_B_GPIO, LED_B_BIT, false);
-
-    /******************/
-
-    Chip_SCU_PinMuxSet(LED_1_PORT, LED_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_1_FUNC);
-    led_1 = DigitalOutputCreate(LED_1_GPIO, LED_1_BIT, false);
-
-    Chip_SCU_PinMuxSet(LED_2_PORT, LED_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_2_FUNC);
-    led_2 = DigitalOutputCreate(LED_2_GPIO, LED_2_BIT, false);
-
-    Chip_SCU_PinMuxSet(LED_3_PORT, LED_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_3_FUNC);
-    led_3 = DigitalOutputCreate(LED_3_GPIO, LED_3_BIT, false);
-
-    /******************/
-
-    Chip_SCU_PinMuxSet(TEC_1_PORT, TEC_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_1_FUNC);
-    tec_1 = DigitalInputCreate(TEC_1_GPIO, TEC_1_BIT, true);
-
-    Chip_SCU_PinMuxSet(TEC_2_PORT, TEC_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_2_FUNC);
-    tec_2 = DigitalInputCreate(TEC_2_GPIO, TEC_2_BIT, true);
-
-    Chip_SCU_PinMuxSet(TEC_3_PORT, TEC_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_3_FUNC);
-    tec_3 = DigitalInputCreate(TEC_3_GPIO, TEC_3_BIT, true);
-
-    Chip_SCU_PinMuxSet(TEC_4_PORT, TEC_4_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_4_FUNC);
-    tec_4 = DigitalInputCreate(TEC_4_GPIO, TEC_4_BIT, true);
+    board_t board = BoardCreate();
 
     /******************/
 
     while (true)
     {
-        if (DigitalInputGetState(tec_1))
+        if (DigitalInputGetState(board->tec_1))
         {
-            DigitalOutputActivate(led_b);
+            DigitalOutputActivate(board->led_b);
         }
         else
         {
-            DigitalOutputDeactivate(led_b);
+            DigitalOutputDeactivate(board->led_b);
         }
 
-        if (DigitalInputHasActivated(tec_2))
+        if (DigitalInputHasActivated(board->tec_2))
         {
-            DigitalOutputToggle(led_1);
+            DigitalOutputToggle(board->led_1);
         }
 
-        if (DigitalInputHasActivated(tec_3))
+        if (DigitalInputHasActivated(board->tec_3))
         {
-            DigitalOutputActivate(led_2);
+            DigitalOutputActivate(board->led_2);
         }
-        if (DigitalInputHasActivated(tec_4))
+        if (DigitalInputHasActivated(board->tec_4))
         {
-            DigitalOutputDeactivate(led_2);
+            DigitalOutputDeactivate(board->led_2);
         }
 
         divisor++;
         if (divisor == 5)
         {
             divisor = 0;
-            DigitalOutputToggle(led_3);
+            DigitalOutputToggle(board->led_3);
         }
 
         for (int index = 0; index < 100; index++)
